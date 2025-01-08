7cc6c2d608ec04485c501dbf723dd8743fc56ca5
umang-tibrewal <87933066+umang-tibrewal@users.noreply.github.com>
2025-01-08 23:20:45 +0530

    final deploy commit

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 window.env = {
-    WEATHER_API_KEY: '0e2c74ceefa7da7df7349ac2f545d27a',
-    SECOND_API_KEY: 'a3b5e6ea46064ea1936b5244907dd63d',
-    TOMORROW_API_KEY: '8xOvkoc8JkPL5UOckcJnNwiAkLF32lcd'
-  };
+    WEATHER_API_KEY: 'your_openweather_api_key_here',
+    SECOND_API_KEY: 'your_geoapify_api_key_here', 
+    TOMORROW_API_KEY: 'your_tomorrow_api_key_here'
+};
